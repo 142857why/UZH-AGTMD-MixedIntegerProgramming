920a100810f36b235201ebbee5b9456dbf4ab076
Rui Zhou <zhourui_1997@126.com>
2021-11-17 15:26:10 +0100

    Finish Task1a)

--- src/IPExercise2a.java
+++ src/IPExercise2a.java
@@ -2,7 +2,9 @@
 import edu.harvard.econcs.jopt.solver.IMIP;
 import edu.harvard.econcs.jopt.solver.IMIPResult;
 import edu.harvard.econcs.jopt.solver.client.SolverClient;
-import edu.harvard.econcs.jopt.solver.mip.MIP;
+import edu.harvard.econcs.jopt.solver.mip.*;
+
+import java.util.ArrayList;
 
 public class IPExercise2a {
 
@@ -20,10 +22,33 @@ public class IPExercise2a {
 
 	private void buildMIP() {
 		mip = new MIP();
+		// TODO: Implement MIP;
+		int n = this.ITEM_VALUES.length;
+		Variable[] a = new Variable[n];
+		Variable[] b = new Variable[n];
+		mip.setObjectiveMax(true);
+		for (int i = 0; i < n; ++i) {
+			a[i] = new Variable("a" + i, VarType.INT, 0, 1);
+			b[i] = new Variable("b" + i, VarType.INT, 0, 1);
+			mip.add(a[i]);
+			mip.add(b[i]);
+			mip.addObjectiveTerm(this.ITEM_VALUES[i], a[i]);
+			mip.addObjectiveTerm(this.ITEM_VALUES[i], b[i]);
+		}
+//		ArrayList<Constraint> c = new ArrayList<>();
+		Constraint c_a = new Constraint(CompareType.LEQ, this.CAPACITY_A);
+		Constraint c_b = new Constraint(CompareType.LEQ, this.CAPACITY_B);
+		for (int i = 0; i < n; ++i) {
+			c_a.addTerm(this.ITEM_WEIGHTS[i], a[i]);
+			c_b.addTerm(this.ITEM_WEIGHTS[i], b[i]);
+			Constraint c_leq1 = new Constraint(CompareType.LEQ, 1);
+			c_leq1.addTerm(1, a[i]);
+			c_leq1.addTerm(1, b[i]);
+			mip.add(c_leq1);
+		}
+		mip.add(c_a);
+		mip.add(c_b);
 
-		/*
-		 * TODO: Implement MIP.
-		 */
 	}
 
 	public IMIPResult solve() {
